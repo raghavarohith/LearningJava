534bc520007f5609c327d21a0c6b2aa85d3d2f40
Raghava <raghavarohit0583@gmail.com>
2022-11-13 14:19:53 +0530

    Add file AccessingPosition character

---
+++ src/main/java/day2/AccessingPosition.java
@@ -0,0 +1,11 @@
+package day2;
+
+public class AccessingPosition {
+    public static void main(String[] args) {
+        String S = "Java Exercises!";
+        char  res=S.charAt(0);char res2=S.charAt(10);
+        System.out.println("The character at position 0 is"+res);
+
+        System.out.println("The character at position 10 is"+res2);
+    }
+}
