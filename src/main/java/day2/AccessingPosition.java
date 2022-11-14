fd8590223da88a6ddc54a66aeb799743dda7b80d
raghavarohith <85346264+raghavarohith@users.noreply.github.com>
2022-11-14 11:38:08 +0530

    Update AccessingPosition.java

--- src/main/java/day2/AccessingPosition.java
+++ src/main/java/day2/AccessingPosition.java
@@ -1,6 +1,14 @@
 package day2;
-
+ 
 public class AccessingPosition {
+    /*
+   * @Author Raghavarohith.N
+   * Date:- 13/11/2022
+   */
+     //Write a Java program to get the character at the given index within the String.
+    //Original String = Java Exercises!
+    //The character at position 0 is J
+    //The character at position 10 is i
     public static void main(String[] args) {
         String S = "Java Exercises!";
         char  res=S.charAt(0);char res2=S.charAt(10);
